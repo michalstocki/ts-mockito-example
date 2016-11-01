7ff6cdcfdcec1bed70e3282c7389059e1ebea695
Michał Stocki <mstocki@ydp.eu>
2016-11-01 14:10:59 +0100

    Initial commit: Typemoq replaced with ts-mockito

--- test/spec/album/AlbumCollectionPaginationProvider.spec.ts
+++ test/spec/album/AlbumCollectionPaginationProvider.spec.ts
@@ -1,19 +1,19 @@
 import {AlbumCollectionPaginationProvider} from '../../../src/album/AlbumCollectionPaginationProvider';
 import {AlbumCollectionProvider} from '../../../src/album/AlbumCollectionProvider';
-import TypeMoq = require('typemoq');
 import {expect} from '../../util/expect';
 import {aPaginationSet} from '../pagination/PaginationSetBuilder';
 import {aPaginationSetItem as aPage} from '../pagination/SimplePaginationSetItemBuilder';
 import {PaginationSetProvider} from '../../../src/pagination/PaginationSetProvider';
+import {when, mock, instance} from 'ts-mockito';
 
 describe('AlbumCollectionPaginationProvider (real logic)', () => {
-    let albumCollectionProvider:TypeMoq.Mock<AlbumCollectionProvider>;
+    let albumCollectionProvider:AlbumCollectionProvider;
     let albumCollectionPaginationProvider:AlbumCollectionPaginationProvider;
 
     beforeEach(() => {
-        albumCollectionProvider = TypeMoq.Mock.ofType(AlbumCollectionProvider);
+        albumCollectionProvider = mock(AlbumCollectionProvider);
         albumCollectionPaginationProvider = new AlbumCollectionPaginationProvider(
-            albumCollectionProvider.object,
+            instance(albumCollectionProvider),
             new PaginationSetProvider()
         );
     });
@@ -21,8 +21,8 @@ describe('AlbumCollectionPaginationProvider (real logic)', () => {
     describe('providing pagination items of albums collection', () => {
         it('returns correct list of available results pages', () => {
             // given
-            albumCollectionProvider.setup(a => a.getCurrentPage()).returns(() => 6);
-            albumCollectionProvider.setup(a => a.getTotalCount()).returns(() => 10);
+            when(albumCollectionProvider.getCurrentPage()).thenReturn(6);
+            when(albumCollectionProvider.getTotalCount()).thenReturn(10);
             const expectedItems = aPaginationSet()
                 .withFirst(aPage(1))
                 .withLast(aPage(10))
@@ -46,16 +46,16 @@ describe('AlbumCollectionPaginationProvider (real logic)', () => {
 });
 
 describe('AlbumCollectionPaginationProvider (mocked logic)', () => {
-    let albumCollectionProvider:TypeMoq.Mock<AlbumCollectionProvider>;
+    let albumCollectionProvider:AlbumCollectionProvider;
     let albumCollectionPaginationProvider:AlbumCollectionPaginationProvider;
-    let paginationSetProvider:TypeMoq.Mock<PaginationSetProvider>;
+    let paginationSetProvider:PaginationSetProvider;
 
     beforeEach(() => {
-        albumCollectionProvider = TypeMoq.Mock.ofType(AlbumCollectionProvider);
-        paginationSetProvider = TypeMoq.Mock.ofType(PaginationSetProvider);
+        albumCollectionProvider = mock(AlbumCollectionProvider);
+        paginationSetProvider = mock(PaginationSetProvider);
         albumCollectionPaginationProvider = new AlbumCollectionPaginationProvider(
-            albumCollectionProvider.object,
-            paginationSetProvider.object
+            instance(albumCollectionProvider),
+            instance(paginationSetProvider)
         );
     });
 
@@ -65,11 +65,9 @@ describe('AlbumCollectionPaginationProvider (mocked logic)', () => {
             const expectedItems = aPaginationSet().build();
             const currentPage = 6;
             const pageCount = 10;
-            albumCollectionProvider.setup(a => a.getCurrentPage()).returns(() => currentPage);
-            albumCollectionProvider.setup(a => a.getTotalCount()).returns(() => pageCount);
-            paginationSetProvider
-                .setup(p => p.getPaginationSet(TypeMoq.It.isValue(currentPage), TypeMoq.It.isValue(pageCount)))
-                .returns(() => expectedItems);
+            when(albumCollectionProvider.getCurrentPage()).thenReturn(currentPage);
+            when(albumCollectionProvider.getTotalCount()).thenReturn(pageCount);
+            when(paginationSetProvider.getPaginationSet(currentPage, pageCount)).thenReturn(expectedItems);
 
             // when
             const result = albumCollectionPaginationProvider.getPaginationItems();
