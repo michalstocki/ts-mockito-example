ebab7a9669fd795149e4eea269f774b8c535cf0b
Michał Stocki <mstocki@ydp.eu>
2017-03-21 12:13:56 +0100

    Added logging from a constructor

--- src/department/it/ITDepartment.ts
+++ src/department/it/ITDepartment.ts
@@ -2,6 +2,10 @@ import {User} from '../../user/User';
 
 export class ITDepartment {
 
+    constructor() {
+        console.info('message from a ITDepartment constructor!');
+    }
+
     public isFriendlyFor(user:User):boolean {
         return null;
     }
